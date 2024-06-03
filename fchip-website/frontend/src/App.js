a9dbe66d7f7523400d8a3cbb1a418fcef3f9b06f
ericz23 <ericz27@stanford.edu>
2024-06-03 13:28:59 -0700

    added HospitalMap and ClinicMap to App.js

--- fchip-website/frontend/src/App.js
+++ fchip-website/frontend/src/App.js
@@ -4,7 +4,7 @@ import SubmitQuery from './map_page/submitQuery';
 import React, {useState, useEffect} from 'react';
 import NavigationBar from './map_page/navbar'; // Assuming NavigationBar is in the same directory
 import ProviderCard from './map_page/providercard';
-import ProviderMap from './map_page/hospitalMap.js';
+import ProviderMap from './map_page/providermap.js';
 import { Dropdown, Row, Col, Button } from 'react-bootstrap';
 import SearchSelector from './map_page/searchSelector.js';
 import { I18nextProvider } from 'react-i18next';
@@ -12,6 +12,8 @@ import LanguageSelector from './map_page/languageSelector.js'
 import { DistanceFilterDropdown, LanguageFilterDropdown } from './map_page/dropdowns.js'
 import i18n from './i18n'; // Import i18n configuration
 import MentalHealthMap from './map_page/mentalHealthMap.js';
+import ClinicMap from './map_page/clinicMap.js';
+import HospitalMap from './map_page/hospitalMap.js';
 
 function App() {
   const [jsonData, setJsonData] = useState('');
